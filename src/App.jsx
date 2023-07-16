c21af9beaaa5dcc2ef668b25604e473112ce69a6
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-16 02:01:16 +0100

    Happy Sunday

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Navbar, Welcome, Footer, Services, Trasaction} from './component';
+
+const App=() => {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
