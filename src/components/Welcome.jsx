3227d5359abb3d9411a962f7b4c0a40a339fc52d
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-19 06:04:22 +0100

    welcome update

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -25,6 +25,9 @@ const Welcome = () => {
              <h1 className="text-3x1 sm:text-5xl text-white text-gradient py-1">
                 Send Crypto <br/> across the world
              </h1>
+             <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+                Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+             </p>
            </div>
         </div>
       </div>
