e81087325252a4be9cd3250b33274fb0cd965ad7
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-19 04:10:31 +0100

    nav update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { HiMenuAlt4} from 'react-icons/hi';
 import { AiOutlineClose} from 'react-icons/ai';
 import PropTypes from 'prop-types'
@@ -20,7 +21,7 @@ NavbarItem.propTypes= {
 }
 
 const Navbar = () => {
-    const[toggleMenu, setToggleMenu] = React.useState(false);
+    const[toggleMenu, setToggleMenu] = useState(false);
     return(
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
 <div className='md:flex-[0.5] flex-initial justify-center items-center'>
