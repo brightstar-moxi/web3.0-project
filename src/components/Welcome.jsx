3cb01e899358443e9eda3f03c15c3ef30b1eda89
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-19 05:56:05 +0100

    welcome updated

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,13 +2,32 @@ import { AiFillPlayCircle} from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import {BsInfoCircle } from 'react-icons/bs';
 
-import { loader} from './';
+//  import { loader} from './';
+
+// const Welcome = () => {
+//     return(
+//       <div className="flex w-full justify-center items-center">
+//         <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-30 px-4">
+//          <div >
+// fsjjkhsfkj
+//          </div>
+//         </div>
+//       </div>
+//     )
+// }
+// export default Welcome
 
 const Welcome = () => {
     return(
-      <div className="flex w-full justisfy">
-
-      </div>>
-    );
+      <div className="flex w-full justify-center items-center">
+        <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+           <div className="flex flex-1 justify-start flex-col md:mr-10">
+             <h1 className="text-3x1 sm:text-5xl text-white text-gradient py-1">
+                Send Crypto <br/> across the world
+             </h1>
+           </div>
+        </div>
+      </div>
+    )
 }
 export default Welcome
