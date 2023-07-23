b0d6c90e4f1d16c1abfef1c1e56524a0e7669b59
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-23 03:42:11 +0100

    up to date

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,18 +4,10 @@ import {BsInfoCircle } from 'react-icons/bs';
 
 //  import { loader} from './';
 const commonStyles ='min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'       
-// const Welcome = () => {
-//     return(
-//       <div className="flex w-full justify-center items-center">
-//         <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-30 px-4">
-//          <div >
-// fsjjkhsfkj
-//          </div>
-//         </div>
-//       </div>
-//     )
-// }
-// export default Welcome
+const Input =({placeholder}) => (
+<input 
+placeholder={placeholder}/>
+);
 
 const Welcome = () => {
   const connectwallet = () => {
@@ -60,11 +52,23 @@ const Welcome = () => {
               <div className='flex justify-between flex-col w-full h-full'>
                  <div className='flex justify-between items-start'>
                    <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
-
+                    <SiEthereum fontSize={21} color="#fff"/>
                    </div>
+                   <BsInfoCircle fontSize={17} color="#fff"/>
+                 </div>
+                 <div>
+                  <p className='text-white font-light text-sm'>
+                   Address
+                  </p>
+                  <p className='text-white font-semibold text-lg mt-1'>
+                  Ethereum
+                  </p>
                  </div>
               </div>
 
+             </div>
+             <div className='p-5 sm-w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+               <input placeholder='Address To' name='addressTo' type='text' handleChange={() => {}}/>
              </div>
            </div>
         </div>
