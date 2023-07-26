67c94f3c51cfe054381e259a6ddefa3ea3bd1fff
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-26 16:24:19 +0100

    update

--- src/context/TransactionContext.js
+++ src/context/TransactionContext.js
@@ -2,3 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
 // import { ContractABI, contractAddress } from '../utils/constants';
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+
+const getethereumContract = () => {
+    const provider = new ethers.provider.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionContract = new ethers.Contract
+
+    console.log({
+        provider,
+        signer,
+    })
+}
